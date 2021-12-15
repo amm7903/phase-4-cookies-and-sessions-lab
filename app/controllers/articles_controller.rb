eca3a6177e7a5919d10d64581b063a2355e2d4d6
Alejandro Marcano <+>
2021-12-15 04:34:51 +0100

    commit tree fdfb9853cdbe995e1b5e6a99d6383732c0669777

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,29 @@ class ArticlesController < ApplicationController
     render json: articles, each_serializer: ArticleListSerializer
   end
 
+  # def show
+  #   article = Article.find(params[:id])
+  #   render json: article
+  # end
+
   def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
     article = Article.find(params[:id])
-    render json: article
+    if session[:page_views] < 3
+      session[:page_views] 
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
+
+  # def current_user
+  #   @_current_user ||= session[:current_user_id] && 
+  #   user.find_by(id: session[:current_user_id])
+  # end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
